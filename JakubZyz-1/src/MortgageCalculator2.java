4e30545e6c615df7333a023bc723c2c1ca3fdd1f
JakubZyz <jakubzyz02@gmail.com>
2024-03-17 20:20:03 +0100

    Java course exercise-1 mortgage calculator fixed

--- JakubZyz-1/src/MortgageCalculator2.java
+++ JakubZyz-1/src/MortgageCalculator2.java
@@ -5,16 +5,14 @@ public class MortgageCalculator2 {
     private static final byte MONTHS = 12;
     private static final byte PERCENT = 100;
     private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("KALKULATOR KREDYTOWY");
-        int principal = (int) readNumber
-                ("Wielkość kredytu (1tys zł  - 1mln zł): ", 1000, 1_000_000);
+        int principal = (int) readNumber("Wielkość kredytu (1tys zł  - 1mln zł): ", 1000, 1_000_000);
 
-        double annualInterest = (double) readNumber
-                ("Podaj roczną stopę procentową: ", 0, 30);
+        double annualInterest = readNumber("Podaj roczną stopę procentową: ", 0, 30);
 
-        int years = (byte) readNumber
-                ("Okres kredytu(w latach): ", 1, 30);
+        int years = (byte) readNumber("Okres kredytu(w latach): ", 1, 30);
 
         printMortgage(principal, annualInterest, years);
 
@@ -25,10 +23,11 @@ public class MortgageCalculator2 {
         double mortgage = calculateMortgage(principal, annualInterest, years);
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String mortgagePln = currency.format(mortgage);
-        System.out.println();
-        System.out.println("KREDYT HIPOTECZNY");
-        System.out.println("-----------------");
-        System.out.println("Miesięczna opłata: " + mortgagePln);
+        System.out.println("""
+                \nKREDYT HIPOTECZNY
+                -----------------
+                Miesięczna opłata:
+                """ + mortgagePln);
     }
 
     private static void printPaymentSchedule(int years, int principle, double annualInterest) {
@@ -56,19 +55,17 @@ public class MortgageCalculator2 {
     public static double calculateAccountValue(int principle, double annualInterest, int years, short paymentsDone) {
         double monthlyInterest = (annualInterest / PERCENT) / MONTHS;
         int numberOfPayments = years * MONTHS;
-        double accountValue = principle *
-                (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, paymentsDone))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        double mortgagePatternNumber = Math.pow(1 + monthlyInterest, numberOfPayments);
+        double mortgagePatternDone =Math.pow(1 + monthlyInterest, paymentsDone);
+        return principle * (mortgagePatternNumber - mortgagePatternDone) / (mortgagePatternNumber - 1);
 
-        return accountValue;
     }
 
     public static double calculateMortgage(int principle, double annualInterest, int years) {
-        double monthyInterest = (annualInterest / PERCENT) / MONTHS;
-        int numerOfPayments = years * MONTHS;
-        double pot = Math.pow(1 + monthyInterest, numerOfPayments);
-        double mortgage = principle * ((monthyInterest * pot) / (pot - 1));
-        return mortgage;
+        double monthlyInterest = (annualInterest / PERCENT) / MONTHS;
+        int numberOfPayments = years * MONTHS;
+        double mortgagePatternNumber = Math.pow(1 + monthlyInterest, numberOfPayments);
+        return principle * ((monthlyInterest * mortgagePatternNumber) / (mortgagePatternNumber - 1));
     }
 
 }
