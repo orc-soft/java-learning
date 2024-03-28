1f701d94b0bd3aa891d8a9d7d8a3ce94713f2ba9
Lork12s <lsork12@gmail.com>
2024-03-28 17:36:41 +0100

    sigma excercise

--- lork12-1/src/MortgageCalc.java
+++ lork12-1/src/MortgageCalc.java
@@ -27,17 +27,19 @@ public class MortgageCalc {
     }
 
     private static void calculateMortgage(int principal, float air, byte period) {
-        double mortgage = principal * ((getMonthlyInterestRate(air) * Math.pow(1 + getMonthlyInterestRate(air), getNumberOfPayments(period))) / (Math.pow(1 + getMonthlyInterestRate(air), getNumberOfPayments(period)) - 1));
+        double mortgageFormulaFirst = Math.pow(1 + getMonthlyInterestRate(air), getNumberOfPayments(period));
+        double mortgage = principal * ((getMonthlyInterestRate(air) * mortgageFormulaFirst) / (mortgageFormulaFirst - 1));
         NumberFormat mortgageValue = NumberFormat.getCurrencyInstance();
         String result = mortgageValue.format(mortgage);
         System.out.println("-------------------------------------\nTwoja miesieczna rata to: " + result);
     }
 
     private static void paymentSchedule(int principal, float air, byte period) {
+        double mortgageFormulaFirst = Math.pow(1 + getMonthlyInterestRate(air), getNumberOfPayments(period));
         double balance;
         System.out.println("-------------------------------------\nTwoj harmonogram rat to: ");
         for (int i = 1; i <= getNumberOfPayments(period); ++i) {
-            balance = principal * ((Math.pow(1 + getMonthlyInterestRate(air), getNumberOfPayments(period))) - (Math.pow(1 + getMonthlyInterestRate(air), i))) / (Math.pow(1 + getMonthlyInterestRate(air), getNumberOfPayments(period)) - 1);
+            balance = principal * ((mortgageFormulaFirst) - (Math.pow(1 + getMonthlyInterestRate(air), i))) / (mortgageFormulaFirst - 1);
             System.out.println(BALANCEVALUE.format(balance));
         }
     }
@@ -45,7 +47,7 @@ public class MortgageCalc {
     private static float getMonthlyInterestRate(float air) {
         return (air / PERCENT) / MONTHS;
     }
-    
+
     private static int getNumberOfPayments(byte period) {
         return period * MONTHS;
     }
