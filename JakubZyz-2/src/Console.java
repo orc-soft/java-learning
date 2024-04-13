964af423549e9cc6122f5f36e0ee3d4e0ba7d255
JakubZyz <jakubzyz02@gmail.com>
2024-04-14 00:26:57 +0200

    +

--- JakubZyz-2/src/Console.java
+++ JakubZyz-2/src/Console.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Console {
     private static final Scanner SCANNER = new Scanner(System.in);
-    public static double readNumber(String promt){
+    public static double readNumber(String prompt){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextDouble();
     }
@@ -15,5 +15,22 @@ public class Console {
                 return value;
             System.out.println("Podaj wartość pomiędzy " + min + " a " + max);
         }
+    }
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj imię i nazwisko: ");
+        return scanner.nextLine();
+    }
+
+    public static String getJob() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj swoją prace: ");
+        return scanner.nextLine();
+    }
+
+    public static String getMonthlySalary() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj miesięczne zarobki: ");
+        return scanner.nextLine();
     }
 }
--- JakubZyz-2/src/MortgageCalculator3.java
+++ JakubZyz-2/src/MortgageCalculator3.java
@@ -1,6 +1,9 @@
 public class MortgageCalculator3 {
     public static void main(String[] args) {
         System.out.println("KALKULATOR KREDYTOWY");
+        /*String fullName = Console.getName();
+        String job = Console.getJob();
+        String monthlySalary = Console.getMonthlySalary();*/
         int principal = (int) Console.readNumber("Wielkość kredytu (1tys zł  - 1mln zł): ", 1000, 1_000_000);
         double annualInterest = Console.readNumber("Podaj roczną stopę procentową: ", 0, 30);
         byte years = (byte) Console.readNumber("Okres kredytu(w latach): ", 1, 30);
